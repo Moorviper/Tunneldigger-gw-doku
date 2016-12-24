0cb7896d36406c16610f099eddb94fb65d4fc2f8
Daniel Krah <moorviper@gmail.com>
2016-12-24 17:06:44 +0100

    basic latex code
    fixed monocodebox
    one issue left (sometimes beaks pygemntize when used breaklines)

---
+++ command/ifconfigVpnBatTD.sh
@@ -0,0 +1,43 @@
+user@host:~$ ifconfig 
+bat0      Link encap:Ethernet  HWaddr 5a:a0:59:f3:f8:fe  
+          inet addr:10.185.0.1  Bcast:10.185.63.255  Mask:255.255.192.0
+          inet6 addr: fe80::58a0:59ff:fef3:f8fe/64 Scope:Link
+          UP BROADCAST RUNNING MULTICAST  MTU:1500  Metric:1
+          RX packets:7305 errors:0 dropped:0 overruns:0 frame:0
+          TX packets:0 errors:0 dropped:8 overruns:0 carrier:0
+          collisions:0 txqueuelen:1000 
+          RX bytes:306810 (306.8 KB)  TX bytes:0 (0.0 B)
+
+eth0      Link encap:Ethernet  HWaddr 52:54:00:73:d5:a0  
+          inet addr:192.168.122.217  Bcast:192.168.122.255  Mask:255.255.255.0
+          inet6 addr: fe80::5054:ff:fe73:d5a0/64 Scope:Link
+          UP BROADCAST RUNNING MULTICAST  MTU:1500  Metric:1
+          RX packets:279804 errors:0 dropped:8 overruns:0 frame:0
+          TX packets:126323 errors:0 dropped:0 overruns:0 carrier:0
+          collisions:0 txqueuelen:1000 
+          RX bytes:3910276319 (3.9 GB)  TX bytes:140584037 (140.5 MB)
+
+lo        Link encap:Local Loopback  
+          inet addr:127.0.0.1  Mask:255.0.0.0
+          inet6 addr: ::1/128 Scope:Host
+          UP LOOPBACK RUNNING  MTU:65536  Metric:1
+          RX packets:162 errors:0 dropped:0 overruns:0 frame:0
+          TX packets:162 errors:0 dropped:0 overruns:0 carrier:0
+          collisions:0 txqueuelen:1 
+          RX bytes:11938 (11.9 KB)  TX bytes:11938 (11.9 KB)
+
+tun0      Link encap:UNSPEC  HWaddr 00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00  
+          inet addr:10.6.0.14  P-t-P:10.6.0.14  Mask:255.255.0.0
+          UP POINTOPOINT RUNNING NOARP MULTICAST  MTU:1500  Metric:1
+          RX packets:0 errors:0 dropped:0 overruns:0 frame:0
+          TX packets:0 errors:0 dropped:0 overruns:0 carrier:0
+          collisions:0 txqueuelen:100 
+          RX bytes:0 (0.0 B)  TX bytes:0 (0.0 B)
+
+tunneldigger Link encap:Ethernet  HWaddr 0a:be:ef:25:00:01  
+          UP BROADCAST PROMISC MULTICAST  MTU:1364  Metric:1
+          RX packets:0 errors:0 dropped:0 overruns:0 frame:0
+          TX packets:0 errors:0 dropped:0 overruns:0 carrier:0
+          collisions:0 txqueuelen:1000 
+          RX bytes:0 (0.0 B)  TX bytes:0 (0.0 B)
+

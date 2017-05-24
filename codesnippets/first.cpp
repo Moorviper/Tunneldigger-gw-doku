b840dac5c4b5653b080f6ebcbdc9ecddf40aec6a
Daniel Krah <moorviper@gmail.com>
2017-05-24 13:09:36 +0200

    test

---
+++ codesnippets/first.cpp
@@ -0,0 +1,9 @@
+using namespace std; // Use Standard Namespace
+#include <iostream> // Include standard iostream classes (trotzdem header)
+
+int main (int argc, char *argv[])
+{
+	// instance of outstream
+	count << "Hello world" << endl; 
+	return 0;
+}

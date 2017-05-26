f6a4d2977e8439be77e5d326bfa095652d07d672
Daniel Krah <moorviper@gmail.com>
2017-05-26 12:05:26 +0200

    restore

--- codesnippets/first.cpp
+++
@@ -1,9 +0,0 @@
-using namespace std; // Use Standard Namespace
-#include <iostream> // Include standard iostream classes (trotzdem header)
-
-int main (int argc, char *argv[])
-{
-	// instance of outstream
-	count << "Hello world" << endl; 
-	return 0;
-}
